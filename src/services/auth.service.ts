8ff33e4c7c93074f03c54964820fedf560119755
Harsh Chouhan <harsh.chouhan@techdome.net.in>
2023-11-17 13:28:53 +0530

    fix cookies set on signup

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -34,9 +34,6 @@ export const register = async (username: string, password: string) => {
     
     const response = await axios.post(`${baseUrl}/auth/register`, raw);
     if (response.status == 201) {
-      const cookies = new Cookies();
-      const data = response.data;
-      cookies.set("token", data?.access_token);
       return true;
     }
     return false;
